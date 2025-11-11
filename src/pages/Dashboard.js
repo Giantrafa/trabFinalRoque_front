746a3a5128dddfd7ba4c9c0b87a331737d7cc996
Giantrafa <giantrafa@gmail.com>
2025-11-11 22:41:12 +0000

    y

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,18 +1,16 @@
+import sampleData from '../data/sampleData';
+
 function Dashboard() {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    //link do banco de dados
-    fetch('../data/sampleData')
-      .then(res => res.json())
-      .then(json => setData(json.slice(0, 4))) // garante 4 gráficos
-      .catch(err => console.error(err));
+    setData(sampleData.slice(0, 4));
   }, []);
 
   return (
     <div className="dashboard-grid">
       {data.map((item) => (
-        <ChartCard key={item.id} data={item} /> // cada gráfico recebe seu próprio item
+        <ChartCard key={item.id} data={item} />
       ))}
     </div>
   );
